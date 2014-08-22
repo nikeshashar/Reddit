20d976cf740896ee15b95a1d13209a3d92e30610
Nikesh Ashar <nikeshashar@gmail.com>
2014-08-22 15:34:36 +0100

    Created Posts Controller and First test

---
+++ spec/features/post_features_spec.rb
@@ -0,0 +1,11 @@
+describe 'posts' do 
+
+	context 'when logged out' do 
+		it 'should take us to a sign up page' do
+			visit '/posts'
+			click_link 'Add a post'
+			expect(page).to have_content 'Sign Up'
+		end
+	end
+
+end
